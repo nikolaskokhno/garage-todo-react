c243f50b7b3a965f870f2f535ebb1097f6092e6f
kokhno <kokhno.mm@gmail.com>
2020-02-08 20:53:20 +0200

    Create Header componnents / add antd, sass

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+
+    return (
+        <div className="header">
+            <h3>simple todo lists</h3>
+            <h5>from ruby garage</h5>
+        </div>
+    );
+}
+
+export default Header;
