16b4dd314278ab6a931f4bfb55f393c3dc272af8
kokhno <kokhno.mm@gmail.com>
2020-02-08 21:03:14 +0200

    Create copyright component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/Header/Header';
+import Copyright from './components/Copyright/Copyright';
 
 const App = () => {
   
   return (
     <div className="container">
       <Header />
+
+      <Copyright />
     </div>
   );
 }
---
+++ src/components/Copyright/Copyright.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Copyright.scss';
+
+const Copyright = () => {
+
+    return (
+        <div className="copyright">
+            &copy; Ruby Garage
+        </div>
+    );
+}
+
+export default Copyright;
