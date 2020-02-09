0f3b4f9eec41dc5c6ffec8b7f1a8baed75235053
kokhno <kokhno.mm@gmail.com>
2020-02-09 12:45:02 +0200

    Create modal window (add new todo list / upgrade ButtonAdd component

--- src/components/ButtonAdd/ButtonAdd.jsx
+++ src/components/ButtonAdd/ButtonAdd.jsx
@@ -1,16 +1,64 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Modal, Input } from 'antd';
 import './ButtonAdd.scss';
 
-const ButtonAdd = () => {
+class ButtonAdd extends React.Component {
 
-    return (
-        <div className="button-add">
-            <Button type="primary" icon="plus" size="large">
-                Add TODO List
-            </Button>
-        </div>
-    );
+    state = {
+        loading: false,
+        visible: false,
+    };
+
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    };
+
+    handleOk = () => {
+        this.setState({ loading: true });
+        setTimeout(() => {
+            this.setState({ loading: false, visible: false });
+        }, 1000);
+    };
+
+    handleCancel = () => {
+        this.setState({ visible: false });
+    };
+
+    render() {
+        const { visible, loading } = this.state;
+        return (
+            <div className="button-add">
+                <Button 
+                    type="primary" 
+                    icon="plus" 
+                    size="large"
+                    onClick={this.showModal}
+                >
+                    Add TODO List
+                </Button>
+
+                <Modal
+                    visible={visible}
+                    title="Create new todo list"
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                    centered
+                    footer={[
+                        <Button key="back" onClick={this.handleCancel}>
+                          Cancel
+                        </Button>,
+                        <Button key="submit" type="primary" loading={loading} onClick={this.handleOk}>
+                          Add todo
+                        </Button>,
+                      ]}
+                    >
+                    <Input placeholder="Start typing here to create a todo..." />
+                </Modal>
+            </div>
+        );
+    }
 }
 
 export default ButtonAdd;
